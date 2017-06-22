fb2c1bf54c0404fb8418a1a3a4b3e7ff90424b2c
Luca Di Stasio <distasio1@univ-lorraine.fr>
2017-06-22 19:17:45 +0200

    committing changes on 22/6/2017 at 19:17:32

--- matlab/matlab2codeaster/writeCODEASTERmeshfile.m
+++ matlab/matlab2codeaster/writeCODEASTERmeshfile.m
@@ -1,21 +1,23 @@
-function[]=writeABQnodegen(filepath,N0,deltaN,nodes,nset)
+function[]=writeCODEASTERmeshfile(filepath,title)
+%%
 %==============================================================================
-% Copyright (c) 2016-2017 Universite de Lorraine & Lulea tekniska universitet
+% Copyright (c) 2016-2017 Université de Lorraine & Luleå tekniska universitet
 % Author: Luca Di Stasio <luca.distasio@gmail.com>
 %                        <luca.distasio@ingpec.eu>
 %
 % Redistribution and use in source and binary forms, with or without
 % modification, are permitted provided that the following conditions are met:
-% 
+%
+%
 % Redistributions of source code must retain the above copyright
 % notice, this list of conditions and the following disclaimer.
 % Redistributions in binary form must reproduce the above copyright
 % notice, this list of conditions and the following disclaimer in
 % the documentation and/or other materials provided with the distribution
-% Neither the name of the Université de Lorraine or Luleå tekniska universitet
+% Neither the name of the Universitï¿½ de Lorraine or Luleï¿½ tekniska universitet
 % nor the names of its contributors may be used to endorse or promote products
 % derived from this software without specific prior written permission.
-% 
+%
 % THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 % AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 % IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -30,29 +32,16 @@ function[]=writeABQnodegen(filepath,N0,deltaN,nodes,nset)
 %==============================================================================
 %
 %  DESCRIPTION
-%  
-%  A function to write  to Abaqus .inp file format.
 %
+% A function to write Code Aster input files.
 %%
 
-fileId = fopen(filepath, 'a');
+writeCODEASTERcreatefile(filepath);
 
-fprintf(fileId,'**\n');
+writeCODEASTERtitre(filepath,title);
 
-rows = size(nodes,1);
-columns = size(nodes,2);
 
-fprintf(fileId,strcat('*NODE, NSET = ',nset,'\n'));
-for i=1:rows
-    line = [' ',num2str(N0+(i-1)*deltaN)];
-    for j=1:columns
-        line = [line,', ',num2str(nodes(i,j))];
-    end
-    fprintf(fileId,[line,'\n']);
-end
 
-fprintf(fileId,'**\n');
-
-fclose(fileId);
+writeCODEASTERfinsf(filepath);
 
 return
